2166b4b1268951022682cb64adbc8331f8332dc3
阿涛 <1370616660@qq.com>
2019-04-13 14:30:50 +0800

    完成了rabbitmq

--- shop_web/shop_item/src/main/java/com/qf/listen/RabbitListen.java
+++ shop_web/shop_item/src/main/java/com/qf/listen/RabbitListen.java
@@ -1,46 +1,42 @@
-package com.qf.controller;
+package com.qf.listen;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.qf.entity.Goods;
 import com.qf.service.IGoodsService;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
-@RequestMapping("/item")
-public class ItemController {
-
+@Component
+public class RabbitListen {
     @Autowired
     private Configuration configuration;
     @Reference
     private IGoodsService goodsService;
-    @RequestMapping("/createHtml")
-    public String createHtml(int gid, HttpServletRequest request){
-        Goods goods = goodsService.queryById(gid);
+    @RabbitListener(queues = "goods2_queue")
+    public void exchange(Goods goods){
+
+
         String[] images=goods.getGimage().split("\\|");
-        String path=this.getClass().getResource("/static/page/").getPath()+goods.getId()+".html";
+
         try {
-            FileWriter fileWriter = new FileWriter(path);
+
             Template template = configuration.getTemplate("goodsitem.ftl");
             Map<String,Object> map=new HashMap<>();
             map.put("goods",goods);
+
             map.put("images",images);
-            map.put("context",request.getContextPath());
+            String path=this.getClass().getResource("/static/page/").getPath()+goods.getId()+".html";
             template.process(map,new FileWriter(path));
-            fileWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return "";
     }
 }
